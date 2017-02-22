5e1869fb80c866820e36f859cc98909bb26a9ff1
AlexandreMelano <alexandremelano@yahoo.com>
2017-02-22 12:50:49 -0500

    Person class update

---
+++ test/CarTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User1
+ */
+public class CarTest {
+    
+    public CarTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    
+    @Test
+    public void testGetMake()
+    {
+          System.out.println("sum with different numbers");
+        int num1 = 10;
+        int num2 = 100;
+        SillyTest instance = new SillyTest();
+        int expResult = 110;
+        int result = instance.sum(num1, num2);
+        assertEquals(expResult, result);
+    }
+}//end of test class
